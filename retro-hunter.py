a62c2dd4ea3a89a8f20bebc7d9214e9a93bb32f7
YAMT <94518822+yetanothermightytool@users.noreply.github.com>
2025-11-16 12:33:04 +0100

    v2.3

--- retro-hunter.py
+++ retro-hunter.py
@@ -335,8 +335,8 @@ def do_mount_scan(token, scanhost, local_ip, rp_id, hostname, use_iscsi, workers
            #else:
                #print(f"[{hostname}] ⚠️ Skipping EVTX scan – no Security.evtx at {evtx_file}")
            if args.regscan:
-              run_regscan(path, hostname, rp_id, rp_ts, rp_status)
-
+                  run_regscan(path, hostname, rp_id, rp_ts, rp_status)
+ 
     time.sleep(10)
     print(f"[{hostname}] 🛑 Unpublishing...")
     time.sleep(3)
